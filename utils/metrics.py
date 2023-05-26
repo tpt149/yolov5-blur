45ab559c18025e8ee9f3f5b25f56eae5c7801bf9
Thao Tran <88588832+tpt149@users.noreply.github.com>
2023-05-26 14:02:42 +0700

    Update metrics.py
    Change ConfusionMatrix normalize to False
    
    Signed-off-by: Thao Tran <88588832+tpt149@users.noreply.github.com>

--- utils/metrics.py
+++ utils/metrics.py
@@ -184,7 +184,7 @@ class ConfusionMatrix:
         return tp[:-1], fp[:-1]  # remove background class
 
     @TryExcept('WARNING ⚠️ ConfusionMatrix plot failure')
-    def plot(self, normalize=True, save_dir='', names=()):
+    def plot(self, normalize=False, save_dir='', names=()):
         import seaborn as sn
 
         array = self.matrix / ((self.matrix.sum(0).reshape(1, -1) + 1E-9) if normalize else 1)  # normalize columns
